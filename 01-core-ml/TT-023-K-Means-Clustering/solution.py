f59d73998531d9a5c499c0dfe1377d0dec3166b1
Shahriar Zeem <164886625+mutassimalzeem@users.noreply.github.com>
2026-05-04 10:22:53 -0700

    Clustering

---
+++ 01-core-ml/TT-023-K-Means-Clustering/solution.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def distance(x1, y1):
+    return np.sum((np.array(x1) - np.array(y1))**2)
+
+def k_means_assignment(points, centroids):
+    labels = []
+    
+    for j in range(len(points)):
+        dist = []
+        for centroid in centroids:
+            dis = distance(points[j], centroid)
+            dist.append(dis)
+        
+        nearest = int(np.argmin(dist))
+        labels.append(nearest)
+    
+    # Returns a flat list of integers
+    return labels
